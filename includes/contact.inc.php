f9b434c8ace5ef1469afbfcc203ce33b90a71c69
laurareboux <rebouxlaura@gmail.com>
2019-12-11 10:22:08 +0100

    changement

--- includes/contact.inc.php
+++ includes/contact.inc.php
@@ -1,10 +1,10 @@
 <h1>contact</h1>
 <?php
-require 'frmContact.php';
 
 if (isset($_POST['frmContact'])) {
   echo "je viens du formulaire";
 }
+
 else {
-  echo "je viens du futur";
+  require 'frmContact.php';
 }
